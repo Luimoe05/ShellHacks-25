8fe2e7d4d104647356414800d9a82eb1b3b28d46
Luimoe05 <lmoreno00528@gmail.com>
2025-09-27 09:29:45 -0400

    Added the ai functionality, just a basic implementation, changed the imports in the app.js server to ES module

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,14 @@
-const express = require("express");
+// OLD: const express = require("express");
+import express from "express";
+
+// OLD: const cors = require("cors");
+import cors from "cors";
+
+// OLD: require("dotenv").config();
+import "dotenv/config";
+import { getAiExplanation } from "./gemini-service.js";
+
 const app = express();
-const cors = require("cors");
-require("dotenv").config();
 
 // Middleware to parse JSON
 app.use(cors({ origin: process.env.FRONTEND_URL }));
@@ -9,6 +16,23 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({ message: "Server is running" });
+});
+
+app.get("/gemini", async (req, res) => {
+  try {
+    const prompt = "Explain how AI works in a few words";
+    const explanation = await getAiExplanation(prompt);
+    console.log(explanation);
+    // Send the actual data back to the client
+    res.json({ success: true, explanation: explanation });
+  } catch (error) {
+    // Error handling from the service layer
+    console.error(error.message);
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
 });
 
 const PORT = process.env.PORT;
