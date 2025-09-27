41c0cec27d42218033f08444676d36c1526ffd11
Ousmanbah10 <ousmanbah3903244@gmail.com>
2025-09-27 14:22:29 -0400

    Slides

---
+++ frontend/src/Components/FinancialUserInputSlide.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function FinancialUserInputSlide() {
+  return (
+    <div>
+      <h2>Financial User Input Slide</h2>
+      {/* Add your form fields here */}
+    </div>
+  );
+}
